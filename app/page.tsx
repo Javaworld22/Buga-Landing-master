2c8783d05b2d0517a4c30878b3962de71d3ae4aa
Javaworld22 <iheruomemichael@gmail.com>
2026-06-29 06:01:37 +0100

    updated the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import Navigation from "@/components/ui/Navigation";
-import HeroSection from "@/components/sections/Hero";
+//import HeroSection from "@/components/sections/Hero";
 import HeroSection1 from "@/components/sections/HeroSection";
-import { Transform } from "@/components/sections/Transform";
+//import { Transform } from "@/components/sections/Transform";
 import HowItWorksSection from "@/components/sections/HowItWorks";
-import { Features } from "@/components/sections/Features";
+//import { Features } from "@/components/sections/Features";
 import { WhatSetUsApart } from "@/components/sections/WhatSetUsApart";
 import { TrustedAndSecure } from "@/components/sections/TrustedAndSecure";
-import { WhatOurWinnerSay } from "@/components/sections/WhatOurWinnerSay";
+//import { WhatOurWinnerSay } from "@/components/sections/WhatOurWinnerSay";
 import { FAQ } from "@/components/sections/FAQ";
 import { Support } from "@/components/sections/Support";
-import  Footer  from "@/components/sections/Footer";
+//import  Footer  from "@/components/sections/Footer";
 import { GamesSection } from "@/components/sections/Games";
 import LatestResultsPage  from "@/components/sections/resultPage";
 
