55ad11a10ee06ae429604050ec01fa890a55e23e
Javaworld22 <iheruomemichael@gmail.com>
2026-08-06 03:24:28 +0100

    commit 06.08.2026_3

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -23,8 +23,11 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
                 fbq('track', 'PageView');
             `}
             </Script>
-            <noscript><img height="1" width="1" style="display:none"
-                src="https://www.facebook.com/tr?id=1300219818815613&ev=PageView&noscript=1"
+            <noscript>
+                <img height="1"
+                width="1"
+            style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=1300219818815613&ev=PageView&noscript=1`}
             /></noscript>
         </>
     );
